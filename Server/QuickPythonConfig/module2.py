afc8b4dbe5f410c1344188177a71ddb9e53f4cbe
zcc <listenzcc@mail.bnu.edu.cn>
2020-11-10 15:17:10 +0800

    Re-add submodule

--- Server/QuickPythonConfig/module2.py
+++
@@ -1,9 +0,0 @@
-# Example of using default CONFIG object
-from Package import CONFIG
-
-
-def main():
-    CONFIG.logger.info('Module 2 is running.')
-    CONFIG.logger.debug('Module 2 is running.')
-
-    CONFIG.set('Module 2', 'says', 'Hello, there')
