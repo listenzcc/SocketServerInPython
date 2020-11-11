318669f7160d44786a815a4343cecedb6a7ee2e1
zcc <listenzcc@mail.bnu.edu.cn>
2020-11-11 17:12:42 +0800

    Add contents into defaultSrc of HTTP

---
+++ Server/HTTP/DefaultSrc/fetch_useful_js.py
@@ -0,0 +1,48 @@
+# File: fetch_useful_js.py
+# Aim: Fetch useful js libraries
+
+# %%
+import configparser
+import os
+import pandas as pd
+import requests
+import time
+
+
+def beside(fname):
+    return os.path.join(os.path.dirname(__file__), fname)
+
+
+def unwrap(content):
+    return content[1:-1]
+
+
+# %%
+cp = configparser.ConfigParser()
+cp.read(beside('useful_js.ini'))
+cp.sections()
+
+# %%
+option = 'user-agent'
+headers = {option: unwrap(cp.get('Headers', option))}
+table = pd.DataFrame()
+URLs = cp['URLs']
+for option in URLs:
+    table = table.append(
+        dict(Name=option, URL=unwrap(URLs[option]), Date=time.ctime()), ignore_index=True)
+table.to_html('Known_js.html')
+
+
+# %%
+for j in table.index:
+    se = table.iloc[j]
+    name, url = se.Name, se.URL
+    fname = os.path.basename(url)
+    print(f'Fetching {name}, {fname}: {url}')
+    got = requests.get(url)
+    text = got.text
+    with open(beside(fname), 'wb') as f:
+        f.write(text.encode('utf-8'))
+
+
+# %%
