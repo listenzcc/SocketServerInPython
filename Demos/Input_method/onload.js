555a34ca74dd4a85a6f0b09f91f1d82ad7bd52c1
zcc <listenzcc@mail.bnu.edu.cn>
2020-11-25 10:35:26 +0800

    Daily update

--- Demos/Input_method/onload.js
+++ Demos/Input_method/onload.js
@@ -5,10 +5,10 @@ console.log(d3, d3.version);
 
 // Add change handler to input area
 // d3.select("#main_input").attr("oninput", "update_input()");
-let input = document.getElementById("main_input");
+let input = document.getElementById('main_input');
 input.onkeydown = handle_input;
 function handle_input(e) {
-    console.log(e.keyCode + " is pressed.");
+    console.log(e.keyCode + ' is pressed.');
     function disable_the_input(e) {
         // Prevent the input event from bubbling
         e.keyCode = 0;
@@ -46,11 +46,9 @@ function handle_input(e) {
             var pair = stack_pop();
             var pinYin = pair[0];
             var ciZu = pair[1];
-            var value = document.getElementById("main_input").value;
-            document.getElementById("main_input").value =
-                value.slice(0, stack_ciZu_length) +
-                pinYin +
-                value.slice(stack_ciZu_length + ciZu.length);
+            var value = document.getElementById('main_input').value;
+            document.getElementById('main_input').value =
+                value.slice(0, stack_ciZu_length) + pinYin + value.slice(stack_ciZu_length + ciZu.length);
             update_options();
         }
         return;
@@ -60,27 +58,27 @@ function handle_input(e) {
     // enter the contents in input and stack into buffered_message area,
     // then clear the options
     if (e.keyCode == 13) {
-        if (document.getElementById("main_input").value.length == 0) {
+        if (document.getElementById('main_input').value.length == 0) {
             // Update options
             update_options();
             disable_the_input(e);
             return;
         }
         // Move main_input value into buffered_message area
-        let textarea = document.getElementById("buffered_message");
-        textarea.innerHTML += document.getElementById("main_input").value;
-        document.getElementById("main_input").value = "";
+        let textarea = document.getElementById('buffered_message');
+        textarea.innerHTML += document.getElementById('main_input').value;
+        document.getElementById('main_input').value = '';
 
         // Tell backend to update the dict
         var pair = stack_popAll();
         var pinYin = pair[0];
         var ciZu = pair[1];
-        console.log("Record user dict: " + pinYin + "=" + ciZu);
-        d3.json("pinYinUpdate?pair=" + pinYin + "," + ciZu).then(function (
-            got
-        ) {
-            console.log(got);
-        });
+        if (pinYin.length > 0) {
+            console.log('Record user dict: ' + pinYin + '=' + ciZu);
+            d3.json('pinYinUpdate?pair=' + pinYin + ',' + ciZu).then(function (got) {
+                console.log(got);
+            });
+        }
 
         // Update options
         update_options();
@@ -118,7 +116,7 @@ function handle_input(e) {
     // only valid if the input area is empty,
     // otherwise, it will only trigger the function of update_input
     if (e.keyCode > 90 || e.keyCode < 65) {
-        if (document.getElementById("main_input").value.length > 0) {
+        if (document.getElementById('main_input').value.length > 0) {
             disable_the_input(e);
         }
         update_options();
@@ -127,37 +125,37 @@ function handle_input(e) {
 
 function select_option(option) {
     // Select [option]
-    var ciZu = option["ciZu"][0];
-    var split = option["full"].split("'");
+    var ciZu = option['ciZu'][0];
+    var split = option['full'].split("'");
     stack_push(split[0], ciZu);
-    document.getElementById("main_input").value = stack_getAllCiZu() + split[1];
+    document.getElementById('main_input').value = stack_getAllCiZu() + split[1];
 }
 
 function refresh_suggestion_quick() {
     // Refresh the quick suggestion area
     // Clear quick suggestions area
-    clearChildren(document.getElementById("suggestion_quick"));
+    clearChildren(document.getElementById('suggestion_quick'));
 
     // Add quick suggestions
-    d3.select("#suggestion_quick")
-        .selectAll("p")
+    d3.select('#suggestion_quick')
+        .selectAll('p')
         .data(quick_option)
         .enter()
-        .append("p")
+        .append('p')
         .text(function (d, i) {
-            return i + "," + option_getById(quick_page_idx * 10 + i, "quick");
+            return i + ',' + option_getById(quick_page_idx * 10 + i, 'quick');
         })
-        .attr("class", "suggestDom");
+        .attr('class', 'suggestDom');
 }
 
 function update_options() {
-    var value = document.getElementById("main_input").value;
+    var value = document.getElementById('main_input').value;
     console.log(value);
 
     // Clear suggestion areas
-    clearChildren(document.getElementById("suggestion_option"));
-    clearChildren(document.getElementById("suggestion_group"));
-    clearChildren(document.getElementById("suggestion_quick"));
+    clearChildren(document.getElementById('suggestion_option'));
+    clearChildren(document.getElementById('suggestion_group'));
+    clearChildren(document.getElementById('suggestion_quick'));
     option_clear();
 
     // Parse valid pinYin input
@@ -170,7 +168,7 @@ function update_options() {
     }
 
     // Check out pinYin input and fill "suggestion area"
-    d3.json("pinYinCheckOut?query=" + value).then(function (rawdata) {
+    d3.json('pinYinCheckOut?query=' + value).then(function (rawdata) {
         // Show what we got
         console.log(rawdata);
 
@@ -179,27 +177,28 @@ function update_options() {
         for (var i in rawdata.Candidates) {
             for (var j in rawdata.Candidates[i]) {
                 var _option = [];
-                _option["short"] = rawdata.Prefix[i] + "'" + rawdata.Remain[i];
-                _option["full"] = rawdata.Full[i];
-                _option["ciZu"] = rawdata.Candidates[i][j];
+                _option['short'] = rawdata.Prefix[i] + "'" + rawdata.Remain[i];
+                _option['full'] = rawdata.Full[i];
+                _option['ciZu'] = rawdata.Candidates[i][j];
                 option_add(_option);
             }
         }
 
         // ----------------------------------------------------------
         // Add options and makeup groups
-        d3.select("#suggestion_option")
-            .selectAll("p")
+        d3.select('#suggestion_option')
+            .selectAll('p')
             .data(all_option)
             .enter()
-            .append("p")
+            .append('p')
             .text(function (d, i) {
-                return option_getById(i, "normal");
+                return option_getById(i, 'normal');
             })
-            .attr("class", "suggestDom")
-            .on("click", function (e, opt) {
+            .attr('class', 'suggestDom')
+            .on('click', function (e, opt) {
                 select_option(opt);
                 update_options();
+                document.getElementById('main_input').focus();
             });
 
         quickOption_move(-1);
